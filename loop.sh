aece4056f055b31602bfcd23a08be44e797b1ee6
Syerne <shubhamyerne1994@gmail.com>
2022-05-26 16:26:23 +0530

    Added: Harmonic number print in series

--- loop.sh
+++ loop.sh
@@ -2,7 +2,6 @@ echo "welcome"
 
 
 #prints a table of the powers of 2 that are less than or equal to 2^n.
-
 read -p "enter power: " p
 num=1
 for((i=1 ; i<=$p ; i++))
@@ -10,3 +9,17 @@ do
 	num=$(( 2*num ))
 	echo $num
 done
+
+
+
+#Harmonic Number upto nth form
+read -p "enter nth form of number: " num
+add=1/1
+for((i=2 ; i<=$num ; i++))
+do
+	add="$add + 1/$i"
+done
+	echo "Harmonic Number= $add + ..... + 1/n"
+
+
+
