dd82da7a7cacefb21724fda5f38e21219d648873
Syerne <shubhamyerne1994@gmail.com>
2022-05-26 16:11:03 +0530

    Added: power of 2^n program

--- loop.sh
+++ loop.sh
@@ -1,1 +1,12 @@
 echo "welcome"
+
+
+#prints a table of the powers of 2 that are less than or equal to 2^n.
+
+read -p "enter power: " p
+num=1
+for((i=1 ; i<=$p ; i++))
+do
+	num=$(( 2*num ))
+	echo $num
+done
